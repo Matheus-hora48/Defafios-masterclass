eb3350b1bb7b9f023cb0a16c5632129ddd9451bb
Matheus Hora <matheus11hora@gmail.com>
2022-10-30 21:08:11 -0300

    Novos

--- componentes/lib/pages/tinder.dart
+++ componentes/lib/pages/tinder.dart
@@ -34,6 +34,7 @@ class TinderPage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Image.asset(
                       'assets/imgs/tinder_icon.png',
@@ -51,7 +52,184 @@ class TinderPage extends StatelessWidget {
                       ),
                     )
                   ],
-                )
+                ),
+                const SizedBox(height: 70),
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(
+                    style: TextStyle(
+                      height: 1.1,
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    children: [
+                      TextSpan(
+                        text:
+                            'By tapping Create Account or Sign In, you agree to our\n',
+                      ),
+                      TextSpan(
+                        text: 'Terms',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                      TextSpan(
+                        text: '. Learn hpw we process your data in our ',
+                      ),
+                      TextSpan(
+                        text: 'Privacy\n',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'Policy ',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                      TextSpan(
+                        text: ' and ',
+                      ),
+                      TextSpan(
+                        text: 'Cookies Policy',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 25),
+                Container(
+                  height: 52,
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(25),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 2,
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: SizedBox(
+                          height: 16,
+                          width: 16,
+                          child: Image.asset('assets/imgs/apple_icon.png'),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        child: const Text(
+                          'SING IN WITH APPLE',
+                          style: TextStyle(
+                            height: 1.5,
+                            letterSpacing: 1.2,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  height: 52,
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(25),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 2,
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: SizedBox(
+                          height: 16,
+                          width: 16,
+                          child: Image.asset('assets/imgs/facebook_icon.png'),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        child: const Text(
+                          'SING IN WITH FACEBOOK',
+                          style: TextStyle(
+                            height: 1.5,
+                            letterSpacing: 1.2,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  height: 52,
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(25),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 2,
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: SizedBox(
+                          height: 16,
+                          width: 16,
+                          child:
+                              Image.asset('assets/imgs/speech_bubble_icon.png'),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        child: const Text(
+                          'SING IN WITH PHONE NUMBER',
+                          style: TextStyle(
+                            height: 1.5,
+                            letterSpacing: 1.2,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Trouble Signing In?',
+                  style: TextStyle(
+                    height: 1.1,
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(height: 20),
               ],
             ),
           ],
